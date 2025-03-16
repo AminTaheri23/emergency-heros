d6269cf51a37d15ac9f1e932efd65dc153c5626d
amintaheri90 <amintaheri90@gmail.com>
2025-03-16 14:56:41 +0000

    added admin page

--- obj/Debug/net8.0/myapp.AssemblyInfo.cs
+++ obj/Debug/net8.0/myapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eda0a7798800f6430a58f19ea1c33617a036f118")]
 [assembly: System.Reflection.AssemblyProductAttribute("myapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
